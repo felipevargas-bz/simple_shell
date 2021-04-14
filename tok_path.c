48eaff921d723a39bd0b9808aedecb152049d176
felipevargas-bz <felipe15angel@gmail.com>
2021-04-14 08:31:53 -0500

    correct/path_size

--- tok_path.c
+++ tok_path.c
@@ -10,9 +10,10 @@ char **token_path(char *path)
 	char **array_of_token = NULL;
 
 	number_routes = count_routes(path);
-	array_of_token = malloc(sizeof(char *) * (number_routes + 1));
+	array_of_token = malloc(sizeof(char *) * (number_routes + 2));
 
     array_of_token[pos] = strtok(path, ": ");
+	pos++;
 
     while ((array_of_token[pos] = strtok(NULL, ": ")))
     {
