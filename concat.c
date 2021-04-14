8bbd87d347fa7096796214841f0b72d54bab4382
mareizaa <marce.3210@gmail.com>
2021-04-14 12:55:42 -0500

    Update

--- concat.c
+++ concat.c
@@ -1,14 +1,16 @@
 #include "header.h"
 /**
- * 
- * 
+ * concat_strings - Concatenates the path with the command
+ * @path_token: Tokenized Route
+ * @arg_token: Tokenized line of argument
+ * Return: Command address the path with command
  */
-char *concat_path_token_and_arg_token(char *path_token, char *arg_token)
+char *concat_strings(char *path_token, char *arg_token)
 {
 	char *command = NULL;
 	int size = 0;
-    int i = 0, concat = 0;
-	
+	int i = 0, concat = 0;
+
 	size = _strlen_command(path_token, arg_token);
 	command = malloc(sizeof(char) * size);
 	while (path_token[i] != '\0')
@@ -16,16 +18,16 @@ char *concat_path_token_and_arg_token(char *path_token, char *arg_token)
 		command[concat] = path_token[i];
 		i++;
 		concat++;
-    }
+	}
 	command[concat] = '/';
-    i = 0;
-    concat++;
-    while (arg_token[i] != '\0')
-    {
+	i = 0;
+	concat++;
+	while (arg_token[i] != '\0')
+	{
 		command[concat] = arg_token[i];
 		i++;
 		concat++;
-    }
+	}
 
-    return (command);
+	return (command);
 }
