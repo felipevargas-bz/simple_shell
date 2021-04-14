a2833c795335ade3b4cde8dba974ca09dc72b2a8
mareizaa <marce.3210@gmail.com>
2021-04-14 12:55:19 -0500

    Update

--- get_path.c
+++ get_path.c
@@ -1,31 +1,31 @@
 #include "header.h"
 /**
- * 
- * 
+ * get_path - Gets the path of the environ
+ * Return: Path
  */
 char *get_path(void)
 {
 	char **pointer_environ = NULL;
 	int i = 0;
-	
+
 	pointer_environ = environ;
-	
+
 	while (*pointer_environ)
-    {
+	{
 		if (!_strncmp(*pointer_environ, "PATH", 4)) /**strncmp = 0*/
-        {
+		{
 			i = _strlen(*pointer_environ);
-            char *path_environ = (char *)malloc(sizeof(char) * i);
+			char *path_environ = (char *)malloc(sizeof(char) * i);
+
 			if (path_environ == NULL)
 			{
 				perror("Error on Path:\n");
 				return (NULL);
 			}
 			_strcpy(path_environ, *pointer_environ);
-			
 			return (not_path(path_environ));
 		}
 		pointer_environ++;
-    }
-    return (NULL);
+	}
+	return (NULL);
 }
