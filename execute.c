2aa380bbc94c90f47b34b36dc5580df18b59dddf
mareizaa <marce.3210@gmail.com>
2021-04-14 13:12:30 -0500

    Update

--- execute.c
+++ execute.c
@@ -1,27 +1,29 @@
 #include "header.h"
 /**
- * 
- * 
+ * execute - creates a child process and executes the commands
+ * @command: Command with route
+ * @arg_token: Arguments
+ * Return: 0 Success
  */
 int execute(char *command, char **arg_token)
 {
-  pid_t pid;
-  
-  if (arg_token == NULL)
-  {
-    return (-1);
-  }
-  pid = fork();
-  wait(NULL);
-  if (pid == -1)
-  {
-    perror("pid Error");
-    return (-1);
-  }
-  else if (pid == 0)
-  {
-    execve(command, arg_token, environ);
-    return (0);
-  }
-  return (-1);
+	pid_t pid;
+
+	if (arg_token == NULL)
+	{
+		return (-1);
+	}
+	pid = fork();
+	wait(NULL);
+	if (pid == -1)
+	{
+		perror("pid Error");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		execve(command, arg_token, environ);
+		return (0);
+	}
+	return (-1);
 }
